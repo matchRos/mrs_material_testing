db240af68eec298b5a26fe7183a9b2a7e3fa80bb
LarsDitt <Lars@dittri.ch>
2025-04-30 18:07:46 +0200

    Update poses.yaml and move_relative_pose.py for improved robot pose handling

--- mrs_material_testing/scripts/move_relative_pose.py
+++ mrs_material_testing/scripts/move_relative_pose.py
@@ -1,45 +1,48 @@
 #!/usr/bin/env python3
 import rospy
-from geometry_msgs.msg import PoseStamped
+from geometry_msgs.msg import Pose, Twist
 from tf.transformations import quaternion_from_euler
+import math
 
 def main():
     rospy.init_node('relative_pose_publisher')
-    pub = rospy.Publisher('/mur620b/UR10_r/relative_pose', PoseStamped, queue_size=10)
+
+    mur620b_UR10l_pub = rospy.Publisher('/mur620b/UR10_l/relative_pose_offset', Pose, queue_size=10)
+    mur620b_UR10r_pub = rospy.Publisher('/mur620b/UR10_r/relative_pose_offset', Pose, queue_size=10)
+    mur620c_UR10l_pub = rospy.Publisher('/mur620c/UR10_l/relative_pose_offset', Pose, queue_size=10)
+    mur620c_UR10r_pub = rospy.Publisher('/mur620c/UR10_r/relative_pose_offset', Pose, queue_size=10)
+    mur620c_UR10l_virtual_object_pub = rospy.Publisher('/mur620b/UR10_l/virtual_object/object_cmd_vel', Twist, queue_size=10)
 
     rate = rospy.Rate(100)  # 100 Hz
-    duration = 5.0  # seconds
+    duration = 10.0  # seconds
     steps = int(duration * 100)  # 500 steps
     wait = int(duration * 1)  # 500 steps
-    y_start = 0.1
-    y_end = 0.35
-    y_step = (y_end - y_start) / steps
-
-    # Startposition
-    x = 0.0007672071499775082
-    z = -0.0010315731500602277
-
-    # Euler zu Quaternion
-    roll = 3.13431331818871
-    pitch = 1.57079632679
-    yaw = 1.57079632679
-    quat = quaternion_from_euler(roll, pitch, yaw)
+    d_start = 0.0
+    d_end = 0.1
+    d_step = (d_end - d_start) / steps
+    phi_start = 0.0
+    phi_end = -90.0
+    omega = ((phi_end - phi_start) / 180 * math.pi) / duration
 
     for i in range(wait):
-        pose_msg = PoseStamped()
-        pose_msg.header.stamp = rospy.Time.now()
-        pose_msg.header.frame_id = "base_link"  # oder passe es an dein Setup an
+        pose_msg = Pose()
+        pose_msg.position.z = 0
+        pose_msg.orientation.x = 0
+        pose_msg.orientation.y = 0
+        pose_msg.orientation.z = 0
+        pose_msg.orientation.w = 1
 
-        pose_msg.pose.position.x = x
-        pose_msg.pose.position.y = y_start
-        pose_msg.pose.position.z = z
 
-        pose_msg.pose.orientation.x = quat[0]
-        pose_msg.pose.orientation.y = quat[1]
-        pose_msg.pose.orientation.z = quat[2]
-        pose_msg.pose.orientation.w = quat[3]
+        # Mur620b UR10l
+        pose_msg.position.x = d_start 
+        pose_msg.position.y = 0
+        mur620b_UR10l_pub.publish(pose_msg)
 
-        pub.publish(pose_msg)
+        # Mur620b UR10r
+        pose_msg.position.x = 0
+        pose_msg.position.y = d_start 
+        mur620b_UR10r_pub.publish(pose_msg)
+
         rate.sleep()
 
     rospy.loginfo("Bewegung abgeschlossen.")
@@ -47,23 +50,34 @@ def main():
     input("Drücke Enter, um die Bewegung zu starten...")
 
     for i in range(steps):
-        pose_msg = PoseStamped()
-        pose_msg.header.stamp = rospy.Time.now()
-        pose_msg.header.frame_id = "base_link"  # oder passe es an dein Setup an
+        pose_msg = Pose()
+        pose_msg.position.z = 0
+        pose_msg.orientation.x = 0
+        pose_msg.orientation.y = 0
+        pose_msg.orientation.z = 0
+        pose_msg.orientation.w = 1
+        twist_msg = Twist()
 
-        pose_msg.pose.position.x = x
-        pose_msg.pose.position.y = y_start + i * y_step
-        pose_msg.pose.position.z = z
 
-        pose_msg.pose.orientation.x = quat[0]
-        pose_msg.pose.orientation.y = quat[1]
-        pose_msg.pose.orientation.z = quat[2]
-        pose_msg.pose.orientation.w = quat[3]
+        # Mur620b UR10l
+        pose_msg.position.x = d_start + i * d_step
+        pose_msg.position.y = 0
+        mur620b_UR10l_pub.publish(pose_msg)
+        twist_msg.angular.x = omega
+        mur620c_UR10l_virtual_object_pub.publish(twist_msg)
+        # Mur620b UR10r
+        pose_msg.position.x = 0
+        pose_msg.position.y = d_start + i * d_step
+        mur620b_UR10r_pub.publish(pose_msg)
 
-        pub.publish(pose_msg)
+        if rospy.is_shutdown():
+            break
+
         rate.sleep()
-
-    rospy.loginfo("Bewegung abgeschlossen.")
+    twist_msg = Twist()
+    mur620c_UR10l_virtual_object_pub.publish(twist_msg)
+    rospy.sleep(0.5)
+    rospy.signal_shutdown("Bewegung abgeschlossen.")
 
 if __name__ == '__main__':
     try:
